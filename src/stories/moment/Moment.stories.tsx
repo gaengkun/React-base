a53c0f08302b6bee2a8a8bf41e0a120538efa0bf
unknown <jaeohkim7@gmail.com>
2020-12-28 04:06:29 +0900

    2020-12-28 kjo Moment Format 추가 보정 필요함

---
+++ src/stories/moment/Moment.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Moment from './Moment';
+
+export default  {
+    title: 'Moment/Legacy',
+    component: Moment
+}
+
+export const Default = () => {
+    return <Moment />;
+}
